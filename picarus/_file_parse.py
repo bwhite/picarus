6057de527690f187ccd520937afe3e4483f50540
Brandyn A. White <bwhite@dappervision.com>
2011-06-26 04:09:38 -0400

    Fixed parser again Project (tp) Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/_file_parse.py
+++ picarus/_file_parse.py
@@ -12,42 +12,36 @@ def _getext(fn):
 def load(fn):
     if not os.path.exists(fn):
         raise IOError('Path does not exist [%s]' % fn)
-    try:
-        ext = _getext(fn)
-    except ValueError:
-        raise ValueError('File needs an extension')
-    if ext[-3:] == '.gz':
+    fn_orig = fn
+    if fn.endswith('.gz'):
         fp = gzip.GzipFile(fn)
-        ext = ext[:-3]
-    elif ext[-4:] == '.bz2':
+        fn = fn[:-3]
+    elif fn.endswith('.bz2'):
         fp = bz2.BZ2File(fn)
-        ext = ext[:-4]
+        fn = fn[:-4]
     else:
         fp = open(fn)
-    if ext == 'js':
+    if fn.endswith('.js'):
         return json.load(fp)
-    elif ext == 'pkl':
+    elif fn.endswith('.pkl'):
         return pickle.load(fp)
     else:
-        raise ValueError('File [%s] has unknown extension [%s]' % (fn, ext))
+        raise ValueError('File [%s] has unknown extension' % fn_orig)
 
 
 def dump(obj, fn):
-    try:
-        ext = _getext(fn)
-    except ValueError:
-        raise ValueError('File needs an extension')
-    if ext[-3:] == '.gz':
+    fn_orig = fn
+    if fn.endswith('.gz'):
         fp = gzip.GzipFile(fn, 'w')
-        ext = ext[:-3]
-    elif ext[-4:] == '.bz2':
+        fn = fn[:-3]
+    elif fn.endswith('.bz2'):
         fp = bz2.BZ2File(fn, 'w')
-        ext = ext[:-4]
+        fn = fn[:-4]
     else:
         fp = open(fn, 'w')
-    if ext == 'js':
+    if fn.endswith('.js'):
         return json.dump(obj, fp)
-    elif ext == 'pkl':
+    elif fn.endswith('.pkl'):
         return pickle.dump(obj, fp, -1)
     else:
-        raise ValueError('File [%s] has unknown extension [%s]' % (fn, ext))
+        raise ValueError('File [%s] has unknown extension' % fn_orig)
