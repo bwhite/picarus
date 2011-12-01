6b3859787b77b8985be1d9ab34e4046779aeec35
Brandyn A. White <bwhite@dappervision.com>
2011-12-01 17:31:25 -0500

    Updated doc pusher Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- doc/push.sh
+++ doc/push.sh
@@ -2,27 +2,34 @@ set -e
 # Build new docs
 rm -rf html
 make html
+rm -rf _build
 
 # Copy relevant files to doc_temp
 pushd ..
 rm -rf doc_temp
-cp -R doc doc_temp
+cp -R doc/html doc_temp
 
 # Ignore the current git changes so we can switch to gh-pages
 git stash
-git checkout gh-pages
+
+# Remove previous files and branch
+#rm -fr _* *.html *.js *.inv
+git branch -d gh-pages
+git checkout --orphan gh-pages
 
 # Ensure that .nojekyll is set for github to ignore _'d dirs
 touch .nojekyll
 git add .nojekyll
 
-# Remove previous docs and update git
-rm -fr _*
-mv ./doc_temp/html/* .
+# Update git
+mv ./doc_temp/* .
 git add -u
+git add *.html
+git add *.js
+git add *.inv
 git add ./_*
 git commit -m "Bump"
-git push
+git push -f
 
 # Switch back to master, revert changes, and cleanup
 git checkout master
