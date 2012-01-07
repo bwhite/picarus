34039e141bbac7edcf27e72405d32d35493a9d82
Brandyn A. White <bwhite@dappervision.com>
2012-01-07 16:50:35 +0000

    Changed to use a queue for local message passing Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/cluster/__main__.py
+++ picarus/cluster/__main__.py
@@ -52,16 +52,34 @@ def run_sample(hdfs_input, hdfs_output, num_clusters, **kw):
                           cmdenvs=['SAMPLE_SIZE=%d' % num_clusters])
 
 
+def _run_local_kmeans(q, num_clusters):
+    # TODO(brandyn): Update command interface
+    import scipy.cluster
+    data = q.get()
+    clusters = sp.cluster.vq.kmeans(data, num_clusters)[0]
+    q.put(clusters)
+
+
+def run_local_kmeans(hdfs_input, hdfs_output, num_clusters, *args, **kw):
+    import multiprocessing
+    data = np.asfarray([y for x, y in hadoopy.readtb(hdfs_input)])
+    q = multiprocess.Queue()
+    p = multiprocessing.Process(target=_run_local_kmeans, args=(q, num_clusters))
+    p.start()
+    p.join()
+    hadoopy.writetb(hdfs_output, enumerate(q.get()))
+
+
 def _run_local_kmeans(hdfs_input, hdfs_output, num_clusters, **kw):
     # TODO(brandyn): Update command interface
     import scipy.cluster
-    data = np.asfarray([y for x, y in hadoopy.readtb(hdfs_input)])
     clusters = sp.cluster.vq.kmeans(data, num_clusters)[0]
     hadoopy.writetb(hdfs_output, enumerate(clusters))
 
 
 def run_local_kmeans(*args, **kw):
     import multiprocessing
+    data = np.asfarray([y for x, y in hadoopy.readtb(hdfs_input)])
     p = multiprocessing.Process(target=_run_local_kmeans, args=args, kwargs=kw)
     p.start()
     p.join()
