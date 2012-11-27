35ebdcac93842b30724280472deb5b704f3df7fc
Brandyn A. White <bwhite@dappervision.com>
2012-11-27 15:10:44 -0500

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/modules/__init__.py
+++ picarus/modules/__init__.py
@@ -129,23 +129,24 @@ class NBNNClassifier(MultiClassClassifier):
     Cons
     """
 
-    def __init__(self, sbin=32, max_side=320, num_points=None, required_files=()):
+    def __init__(self, sbin=32, max_side=320, num_points=None, scale=1, required_files=()):
         self.required_files = required_files
         self.max_side = max_side
         self._feature = self._feature_hog_loc  # self._feature_hist
         self.db = None
+        self.scale = scale
         self.num_points = num_points
         self.classes = None
         self.dist = distpy.L2Sqr()
         self.sbin = sbin
 
-    def _feature_hog_loc(self, image, scale=1):
+    def _feature_hog_loc(self, image):
         feature_mask = imfeat.HOGLatent(self.sbin).compute_dense_2d(image)
         features = []
         norm = np.asfarray(feature_mask.shape[:2])
         for y in range(feature_mask.shape[0]):
             for x in range(feature_mask.shape[1]):
-                yx = np.array([y, x]) / norm * scale
+                yx = np.array([y, x]) / norm * self.scale
                 features.append(np.hstack([feature_mask[y, x, :], yx]))
                 print(features[-1])
         return np.asfarray(features)
