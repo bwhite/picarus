20e1658be466a89e0c706239aed7a6b3527d766d
Brandyn A. White <bwhite@dappervision.com>
2013-04-18 09:07:36 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/js/tabs.js
+++ picarus/api/js/tabs.js
@@ -100,14 +100,11 @@ function render_data_uploads(email_auth) {
         },
         render: function() {
             var startRow = _.unescape(this.model.pescape('upload_row_prefix'));
-            var imageColumn = encode_id('thum:image_150sq');
+            var imageColumn = 'thum:image_150sq';
             function success(row, columns) {
-                //$('#images').append(escape(decode_id(row)));
-                //$('#images').append($('<br>'));
-                $('#images').append($('<img>').attr('src', 'data:image/jpeg;base64,' + columns[imageColumn]).attr('width', '150px'));
-                //$('#images').append($('<br>'));
+                $('#images').append($('<img>').attr('src', 'data:image/jpeg;base64,' + base64.encode(columns[imageColumn])).attr('width', '150px'));
             }
-            PICARUS.scanner("images", startRow, prefix_to_stop_row(startRow), {success: success, maxRows: 24})
+            PICARUS.scanner("images", startRow, prefix_to_stop_row(startRow), {success: success, maxRows: 24, columns: [imageColumn]})
         }
     });
     var model = new PicarusUser({row: encode_id(email_auth.email)});
