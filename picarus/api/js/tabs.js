9aa53c4dd0e5e6e7137cb067b19226876f59ad81
Brandyn A. White <bwhite@dappervision.com>
2013-04-18 12:29:36 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/js/tabs.js
+++ picarus/api/js/tabs.js
@@ -554,9 +554,9 @@ function render_process_exif() {
     row_selector($('#rowPrefixDrop'), $('#startRow'), $('#stopRow'));
     $('#runButton').click(function () {
         button_running();
-        var startRow = encode_id($('#startRow').val());
-        var stopRow = encode_id($('#stopRow').val());
-        picarus_api("/a1/slice/images/" + startRow + '/' + stopRow, "POST", {success: button_reset, fail: button_error, data: {action: 'io/exif'}});
+        var startRow = $('#startRow').val();
+        var stopRow = $('#stopRow').val();
+        PICARUS.postSlice('images', startRow, stopRow, 'io/exif', {success: button_reset, fail: button_error)
     });
 }
 function render_process_modify() {
