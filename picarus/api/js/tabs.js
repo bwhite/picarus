da707af19d8d68907c30a8b17628aec8ccac9330
Brandyn A. White <bwhite@dappervision.com>
2013-04-18 12:39:13 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/js/tabs.js
+++ picarus/api/js/tabs.js
@@ -529,9 +529,9 @@ function render_process_thumbnail() {
     row_selector($('#rowPrefixDrop'), $('#startRow'), $('#stopRow'));
     $('#runButton').click(function () {
         button_running();
-        var startRow = encode_id($('#startRow').val());
-        var stopRow = encode_id($('#stopRow').val());
-        picarus_api("/a1/slice/images/" + startRow + '/' + stopRow, "POST", {success: button_reset, fail: button_error, data: {action: 'io/thumbnail'}});
+        var startRow = $('#startRow').val();
+        var stopRow = $('#stopRow').val();
+        PICARUS.postSlice('images', startRow, stopRow, 'io/thumbnail', {success: button_reset, fail: button_error})
     });
 }
 function render_process_garbage() {
@@ -577,24 +577,24 @@ function render_process_copy() {
     row_selector($('#rowPrefixDrop2'));
     $('#runButton').click(function () {
         button_running();
-        var imageColumn = encode_id('data:image');
-        var columnName = encode_id($('#columnName').val());
+        var imageColumn = 'data:image';
+        var columnName = $('#columnName').val();
         var columnValue = $('#columnValue').val();
-        var startRow = encode_id($('#startRow').val());
-        var stopRow = encode_id($('#stopRow').val());
+        var startRow = $('#startRow').val();
+        var stopRow = $('#stopRow').val();
         var prefix = $('#rowPrefixDrop2 option:selected').val();
         var maxRows = Number($('#maxRows').val());
         function success(row, columns) {
             // Generate row key
-            var cur_row = encode_id(prefix + random_bytes(10));
+            var cur_row = prefix + random_bytes(10);
             cur_data = {};
             cur_data[imageColumn] = columns[imageColumn];
             if (columnName.length)
-                cur_data[columnName] = base64.encode(columnValue);
-            picarus_api_row("images", cur_row, "PATCH", {data: cur_data});
+                cur_data[columnName] = columnValue;
+            PICARUS.patchRow("images", cur_row, {data: cur_data});
         }
         // Scan through rows, each one write back to the prefix
-        picarus_api_data_scanner("images", startRow, stopRow, [imageColumn], {success: success, done: button_reset, fail: button_error, maxRows: maxRows, maxRowsIter: 5});
+        PICARUS.scanner("images", startRow, stopRow, {success: success, done: button_reset, fail: button_error, maxRows: maxRows, maxRowsIter: 5, columns: [imageColumn]});
     });
 }
 function render_annotate_list() {
--- picarus/api/tabs/process_thumbnail.js
+++ picarus/api/tabs/process_thumbnail.js
@@ -2,8 +2,8 @@ function render_process_thumbnail() {
     row_selector($('#rowPrefixDrop'), $('#startRow'), $('#stopRow'));
     $('#runButton').click(function () {
         button_running();
-        var startRow = encode_id($('#startRow').val());
-        var stopRow = encode_id($('#stopRow').val());
-        picarus_api("/a1/slice/images/" + startRow + '/' + stopRow, "POST", {success: button_reset, fail: button_error, data: {action: 'io/thumbnail'}});
+        var startRow = $('#startRow').val();
+        var stopRow = $('#stopRow').val();
+        PICARUS.postSlice('images', startRow, stopRow, 'io/thumbnail', {success: button_reset, fail: button_error})
     });
 }
