1148e04520b4af7c95651b37f3f25a939a306167
Brandyn A. White <bwhite@dappervision.com>
2013-04-19 11:44:16 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/build_site.py
+++ picarus/api/build_site.py
@@ -1,5 +1,6 @@
 import glob
 import subprocess
+import argparse
 
 def render_app():
     template_names = 'data_prefixes data_projects data_user data_uploads crawl_flickr models_list models_create models_single models_slice process_thumbnail process_garbage process_exif process_modify process_copy annotate_list annotate_batch annotate_entity visualize_thumbnails visualize_metadata visualize_exif visualize_locations visualize_times visualize_annotations evaluate_classifier'.split()
@@ -16,13 +17,24 @@ def render_app():
     open('js/tabs.js', 'w').write('\n'.join(scripts))
     preinclude_css = ['bootstrap_flat.css', 'flat-ui.css', 'hint.min.css', 'custom.css']
     open('static/style.css', 'w').write('\n'.join([open('css/' + x).read() for x in preinclude_css]))
-render_app()
-preinclude = ['jquery.min.js', 'underscore-min.js']
-preinclude = ['js/' + x for x in preinclude]
 
-a = preinclude + list(set(glob.glob('js/*.js')) - set(preinclude))
 
-a= ' '.join(['--js %s' % x for x in a])
-# --compilation_level ADVANCED_OPTIMIZATIONS
-cmd = 'java -jar compiler.jar %s --js_output_file static/compressed.js' % a
-subprocess.call(cmd.split(' '))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true',
+                        help="Don't minify the source")
+    args = parser.parse_args()
+    render_app()
+    preinclude = ['jquery.min.js', 'underscore-min.js']
+    preinclude = ['js/' + x for x in preinclude]
+    a = preinclude + list(set(glob.glob('js/*.js')) - set(preinclude))
+    if args.debug:
+        open('static/compressed.js', 'w').write('\n'.join([open(x).read() for x in a]))
+    else:
+        a= ' '.join(['--js %s' % x for x in a])
+        cmd = 'java -jar compiler.jar %s --js_output_file static/compressed.js' % a
+        subprocess.call(cmd.split(' '))
+
+
+if __name__ == '__main__':
+    main()
