d4aad7002dfe4067263bcd6733a16388faa8bb72
Brandyn A. White <bwhite@dappervision.com>
2013-04-19 13:42:37 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/js/tabs.js
+++ picarus/api/js/tabs.js
@@ -38,19 +38,19 @@ function render_data_prefixes() {
     rows.fetch();
 }
 function render_data_projects() {
-    rows = new PicarusRows([], {'table': 'projects'});
-    rows_dropdown(rows, {el: $('#prefixTable'), text: function (x) {return x.pescaperow()}});
+    rows = new Picarus2Rows([], {'table': 'projects'});
+    rows_dropdown(rows, {el: $('#prefixTable'), text: function (x) {return x.escape('row')}});
     slices_selector();
     $('#modifyProjectButton').click(function () {
-        var row = $('#prefixTable option:selected').val();
+        var row = decode_id($('#prefixTable option:selected').val());
         var data = {};
         var slices = slices_selector_get(true);
         var value = _.map(slices, function (x) {return x[0] + '/' + x[1]}).join(',');
         data[$('#projectName').val()] = value;
-        rows.get(row).psave(data, {patch: true});
+        rows.get(row).save(data, {patch: true});
     });
     var tableColumn = {header: "Table", getFormatted: function() {
-        return _.escape(base64.decode(this.get('row')));
+        return this.escape('row');
     }};
     new RowsView({collection: rows, el: $('#prefixes'), extraColumns: [tableColumn], deleteValues: true});
     rows.fetch();
