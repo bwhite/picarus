30642a8c2ecbeb09468567cd14f09d73a71ed7bf
Brandyn A. White <bwhite@dappervision.com>
2013-04-23 18:38:49 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ picarus/api/setup_hbase.py
@@ -0,0 +1,24 @@
+import hadoopy_hbase
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Picarus user operations')
+    parser.add_argument('--thrift_server', default='localhost')
+    parser.add_argument('--thrift_port', default='9090')
+    ARGS = parser.parse_args()
+    hb = hadoopy_hbase.connect(ARGS.thrift_server, ARGS.thrift_port)
+    hb.createTable('picarus_models', [hadoopy_hbase.ColumnDescriptor('data:', maxVersions=1, compression='SNAPPY'),
+                                      hadoopy_hbase.ColumnDescriptor('meta:', maxVersions=1),
+                                      hadoopy_hbase.ColumnDescriptor('user:', maxVersions=1)])
+    hb.createTable('images', [hadoopy_hbase.ColumnDescriptor('data:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('meta:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('pred:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('srch:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('attr:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('misc:', maxVersions=1),
+                              hadoopy_hbase.ColumnDescriptor('feat:', maxVersions=1, compression='SNAPPY'),
+                              hadoopy_hbase.ColumnDescriptor('hash:', maxVersions=1)])
+
+if __name__ == '__main__':
+    main()
