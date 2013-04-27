3bd7a4c29ff9c019fb9ea3fc536de12832b7ba2b
Brandyn A. White <bwhite@dappervision.com>
2013-04-27 14:59:24 -0400

    Updated prefix management
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/tabs/data_prefixes.js
+++ picarus/api/tabs/data_prefixes.js
@@ -1,7 +1,7 @@
 function render_data_prefixes() {
     rows = new PicarusRows([], {'table': 'prefixes'});
     function prefixChange() {
-        var row = $('#prefixTable option:selected').val()
+        var row = $('#prefixTable option:selected').val();
         if (_.isUndefined(row)) {
             $('#permissions').html('');
             return;
@@ -38,7 +38,13 @@ function render_data_prefixes() {
     }
     rows_dropdown(rows, {el: $('#prefixTable'), text: function (x) {return x.escape('row')}, change: change});
     $('#createButton').click(function () {
-        var row = decode_id($('#prefixTable option:selected').val());
+        var row = $('#prefixTable option:selected').val();
+        if (_.isUndefined(row)) {
+            $('#prefixDrop').html('');
+            prefixChange();
+            return;
+        }
+        row = decode_id(row);
         var data = {}
         data[decode_id($('#prefixDrop option:selected').val()) + unescape($('#suffix').val())] = decode_id($('#permissions option:selected').val());
         rows.get(row).save(data, {patch: true});        
