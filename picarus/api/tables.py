be4a0bc75187b73d8944ffc595af4d6e9bb2b461
Brandyn A. White <bwhite@dappervision.com>
2013-04-27 23:54:43 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/tables.py
+++ picarus/api/tables.py
@@ -687,7 +687,7 @@ def register_frames(row):
         if mask is None:
             mask = np.ones((frame.shape[0], frame.shape[1]))
         points, descs = brisk.detectAndCompute(frame, mask)
-        points = np.array([(x.x, x.y) for x in points])
+        points = np.array([x.pt for x in points])
         print((frame_num, points, descs))
         if prev_descs is not None:
             matches = (hamming.cdist(prev_descs, descs) < match_thresh).nonzero()
