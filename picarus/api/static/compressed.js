a6677cb5db2f2d0661f70b4b1f0a43eac8d45a31
Brandyn A. White <bwhite@dappervision.com>
2013-05-04 20:31:17 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/static/compressed.js
+++ picarus/api/static/compressed.js
@@ -989,7 +989,9 @@ function render_annotate_entity() {
     });
 }
 function render_workflow_classifier() {
-
+    model_create_selector($('#slices_select'), $('#params_preprocessor'), 'preprocessor', 'preprocessor');
+    model_create_selector($('#slices_select'), $('#params_feature'), 'feature', 'bovw');
+    model_create_selector($('#slices_select'), $('#params_classifier'), 'classifier', 'svmkernel');
 }
 function render_visualize_thumbnails() {
     row_selector($('#rowPrefixDrop'), {startRow: $('#startRow'), stopRow: $('#stopRow')});
--- picarus/api/tabs/workflow_classifier.js
+++ picarus/api/tabs/workflow_classifier.js
@@ -1,3 +1,5 @@
 function render_workflow_classifier() {
-
+    model_create_selector($('#slices_select'), $('#params_preprocessor'), 'preprocessor', 'preprocessor');
+    model_create_selector($('#slices_select'), $('#params_feature'), 'feature', 'bovw');
+    model_create_selector($('#slices_select'), $('#params_classifier'), 'classifier', 'svmkernel');
 }
