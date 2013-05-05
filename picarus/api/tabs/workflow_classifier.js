4ba483e61f096393c02018dac4cab7645099b37f
Brandyn A. White <bwhite@dappervision.com>
2013-05-04 22:40:31 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/tabs/workflow_classifier.js
+++ picarus/api/tabs/workflow_classifier.js
@@ -34,19 +34,14 @@ function render_workflow_classifier() {
             $('#slices_select').append(document.getElementById('bpl_slices_select').innerHTML);
             // NOTE(brandyn): Factory parameters needs more specification of output_type
             // as we can't do this automatically without it, for now it is hardcoded
-            var preprocessors = PARAMETERS.filter(function (x) {
-                return _.contains(['preprocessor'], x.get('name'));
-            });
-            var features = PARAMETERS.filter(function (x) {
-                return _.contains(['bovw', 'picarus.GISTImageFeature', 'picarus.HistogramImageFeature'], x.get('name'));
-            });
-            var classifiers = PARAMETERS.filter(function (x) {
-                return _.contains(['svmlinear', 'svmkernel'], x.get('name'));
-            });
+            var names = PARAMETERS.pluck('name');
+            var preprocessors = _.intersection(names, ['preprocessor']);
+            var features = _.intersection(names, ['bovw', 'picarus.GISTImageFeature', 'picarus.HistogramImageFeature']);
+            var classifiers = _.intersection(names, ['svmlinear', 'svmkernel']);
             var select_template = "{{#models}}<option value='{{.}}'>{{.}}</option>{{/models}};" // text is escaped already
-            $('#preprocess_select').html(Mustache.render(select_template, {models: _.pluck(preprocessors, 'name')}));
-            $('#feature_select').html(Mustache.render(select_template, {models: _.pluck(features, 'name')}));
-            $('#classifier_select').html(Mustache.render(select_template, {models: _.pluck(classifiers, 'name')}));
+            $('#preprocess_select').html(Mustache.render(select_template, {models: preprocessors}));
+            $('#feature_select').html(Mustache.render(select_template, {models: features}));
+            $('#classifier_select').html(Mustache.render(select_template, {models: classifiers}));
             slices_selector();
             this.renderPreprocessor();
             this.renderFeature();
