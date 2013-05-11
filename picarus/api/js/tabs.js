988403d02d39ede3a992b00cf5589b48fc961ad9
Brandyn A. White <bwhite@dappervision.com>
2013-05-11 16:55:42 -0400

    Added delete slice
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- picarus/api/js/tabs.js
+++ picarus/api/js/tabs.js
@@ -426,6 +426,15 @@ function render_process_thumbnail() {
         PICARUS.postSlice('images', startRow, stopRow, {data: {action: 'io/thumbnail'}, success: button_reset, fail: button_error})
     });
 }
+function render_process_delete() {
+    row_selector($('#rowPrefixDrop'), {startRow: $('#startRow'), stopRow: $('#stopRow')});
+    $('#runButton').click(function () {
+        button_running();
+        var startRow = $('#startRow').val();
+        var stopRow = $('#stopRow').val();
+        PICARUS.deleteSlice('images', startRow, stopRow, {success: button_reset, fail: button_error})
+    });
+}
 function render_process_garbage() {
     row_selector($('#rowPrefixDrop'), {startRow: $('#startRow'), stopRow: $('#stopRow')});
     $('#runButton').click(function () {
