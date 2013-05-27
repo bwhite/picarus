60902990bbf1472a02860a180567e78e2352c079
Brandyn A. White <bwhite@dappervision.com>
2013-05-27 18:40:55 -0400

    Fixed typo
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- server/databases.py
+++ server/databases.py
@@ -57,7 +57,7 @@ def job_runner(*args, **kw):
 
 def job_worker(db, method, method_args, method_kwargs):
     print('job_worker: db[%s] method[%s] args[%s] kw[%s]' % (db, method, method_args, method_kwargs))
-    getattr(db, method)(db, *method_args, **method_kwargs)
+    getattr(db, method)(*method_args, **method_kwargs)
 
 
 def async(func):
@@ -66,7 +66,7 @@ def async(func):
         print('async: spawn[%s] [%s] [%s] [%s]' % (self._spawn, func.__name__,
                                                    args, kw))
         if self._spawn is None:
-            return func(*args, **kw)
+            return func(self, *args, **kw)
         self._spawn(job_runner, db=self, method=func.__name__,
                     method_args=args, method_kwargs=kw)
     return inner
